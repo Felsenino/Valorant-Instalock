11384e5ba4e7e695da48ad5c2f64b5abb6259349
Cesare Polidoro <106200957+GitKa3sar@users.noreply.github.com>
2024-02-13 16:55:08 +0100

    added GUI v0.1 in main.py, deleted GUI_test.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyautogui as p
 from time import sleep
+import tkinter as tk
+from tkinter import ttk
 
+'''
 # INSERT HERE THE NAME OF THE AGENT YOU WANT TO PLAY
 USER_AGENT_SELECT = "iso"
 
@@ -25,3 +28,85 @@ sleep(0.01)
 
 p.moveTo(lockButtonX, lockButtonY)
 p.click()
+
+'''
+
+# Bug, once you choose your agent, and it lock-in, DO NOT CHANGE SELECTION, if you do, program will crush, close it instead
+
+
+# debug function
+def log():
+    print("True")
+
+
+def selected(event):
+
+    # INSERT HERE THE NAME OF THE AGENT YOU WANT TO PLAY
+    #USER_AGENT_SELECT = "iso"
+
+
+    # variables 
+    lockButtonX, lockButtonY = 960, 730 #Res must be, 1920x1080
+    #agent = USER_AGENT_SELECT.lower()
+    agent = selector.get()
+    print("Current agent selected:",agent)
+    imgLocation = None
+    imageFile = f'imgs/{agent}.png'
+
+    # when pyautogui finds the image of the agents, returns the coordinates to imgLocation
+    while (imgLocation == None):
+        try:
+            imgLocation = p.locateCenterOnScreen(imageFile, confidence=0.8)
+        except Exception as e:
+            print(e)
+
+
+    p.moveTo(imgLocation)
+    p.click()
+    sleep(0.01)
+
+    p.moveTo(lockButtonX, lockButtonY)
+    p.click()
+
+
+# window cfg
+window = tk.Tk() 
+window.geometry("150x20")
+window.title("Valorant Instalocker v0.1 by Felsen")
+window.resizable(False, False)
+window.configure(background="black")
+
+# selector
+agents = [
+    "astra",
+    "breach",
+    "brimstone",
+    "chamber",
+    "cypher",
+    "deadlock",
+    "fade",
+    "gekko",
+    "harbor",
+    "iso",
+    "jett",
+    "kayo",
+    "killjoy",
+    "neon",
+    "omen",
+    "phoenix",
+    "raze",
+    "reyna",
+    "sage",
+    "skye",
+    "sova",
+    "viper",
+    "yoru"
+]
+
+selector = ttk.Combobox(window, values = agents)
+selector.set("Pick an Option") # bug, can edit this text
+selector.bind("<<ComboboxSelected>>", selected)
+selector.pack()
+
+
+window.mainloop()
